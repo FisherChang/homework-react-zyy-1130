0399c5c4b3e0a0cdb652848b07af5f41c935d592
Yuyu Zhang <1204891354@qq.com>
2022-11-29 09:24:58 +0800

    feat: add sideBar.js and scss

---
+++ src/sideBar/sideBar.js
@@ -0,0 +1,42 @@
+import React from "react";
+import './sideBar.scss';
+import '../ASSETS/font_icons/fonts.css';
+import {Link, BrowserRouter, Route, Routes} from "react-router-dom";
+import Content from "../content/content";
+import HomePage from "../homePage";
+
+function SideBar ({sideItems}){
+    return(
+        <aside className="sideBar">
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/content" element={<Content/>} />
+                    <Route path="/home" element={<HomePage />} />
+                </Routes>
+                <ul className="sideList">
+                    {sideItems.map((item)=>(
+                        <li key={item.id}>
+                            {
+                                item.label==="AGENT"
+                                    ? <Link className="link" to="/content">
+                                        <i className={item.icon}></i>
+                                        {item.label}</Link>
+                                    : <Link className="link" to="/home">
+                                        <i className={item.icon}></i>
+                                        {item.label}</Link>
+                            }
+                        </li>
+                    ))}
+                </ul>
+                <ul className="history-content">
+                    <h3>History</h3>
+                    <ul className="history-info">
+                        <li>bjstdmngbgr02/Acceptance_test</li>
+                    </ul>
+                </ul>
+            </BrowserRouter>
+        </aside>
+    )
+}
+
+export default SideBar;
