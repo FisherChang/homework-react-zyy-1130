ff56359a16c0b9ec309f08b991fb2e7d304ab339
Yuyu Zhang <1204891354@qq.com>
2022-11-30 11:06:07 +0800

    fix: change db.json position to src/

--- src/agent/agent.js
+++ src/agent/agent.js
@@ -1,18 +1,12 @@
 
 import React from "react";
 import './agent.scss';
-import debin from '../ASSETS/os_icons/debin.png';
-import windows from '../ASSETS/os_icons/windows.png';
 import AgentDetails from "./agentDetails";
-import agents from '../../mock-server/db.json';
+import agents from '../db.json';
 
 function Agent (){
     return(
         <section className="agent">
-            <img src={debin} alt="deb" />
-            <img src={windows} alt="win" />
-
-
             <AgentDetails agents={agents}/>
         </section>
 
