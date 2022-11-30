ce99ef687be320dfa9dbedb4d26e642c3ec80b2f
Yuyu Zhang <1204891354@qq.com>
2022-11-30 14:14:32 +0800

    fix: require os img in agent page

--- src/agent/agentDetails.js
+++ src/agent/agentDetails.js
@@ -1,26 +1,52 @@
 import React from "react";
 import "../ASSETS/font_icons/fonts.css";
-import windows from "../ASSETS/os_icons/windows.png";
+import "./agentDetails.scss";
 
 const AgentDetails = ({ agents }) => {
+  // console.log(agents);
+
   return (
     <section className="agentList">
       {agents.map(({id, ip, location, name, os, resources, status}) => (
         <div className="agentItem" key={id}>
           <figure>
-            <img src={windows} alt={os} />
+            <img src={require(`../ASSETS/os_icons/${os}.png`)} alt={os} />
           </figure>
-          <div className="agentInfo">
-            <i className="icon-desktop"></i>
-            <h4>{name}</h4>
-            <p className="status">{status}</p>
-            <i className="icon-info"></i>
-            <p className="ip">{ip}</p>
-            <i className="icon-folder"></i>
-            <p className="location">{location}</p>
-          </div>
-          <div className="agentTypeList">
-            <p>{resources}</p>
+          <div className="agentContent">
+            <div className="agentInfo">
+              <i className="icon-desktop"></i>
+              <h4>{name}</h4>
+              <p className="status">{status}</p>
+              <i className="icon-info"></i>
+              <p className="ip">{ip}</p>
+              <i className="icon-folder"></i>
+              <p className="location">{location}</p>
+            </div>
+            <div className="agentTypeList">
+              <i className="icon-plus"></i>
+              <div className="resource-list">
+                <div className="resource-item">
+                  <span>{resources[0]}</span>
+                  <i className="icon-trash"></i>
+                </div>
+                <div className="resource-item">
+                  <span>{resources[1]}</span>
+                  <i className="icon-trash"></i>
+                </div>
+                <div className="resource-item">
+                  <span>{resources[2]}</span>
+                  <i className="icon-trash"></i>
+                </div>
+                <div className="resource-item">
+                  <span>{resources[3]}</span>
+                  <i className="icon-trash"></i>
+                </div>
+              </div>
+              <div className="deny">
+                <i className="icon-deny"></i>
+                <span>Deny</span>
+              </div>
+            </div>
           </div>
         </div>
       ))}
