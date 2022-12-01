38e917913aa155d9b19ca91ba8c18bd00e221faa
Yuyu Zhang <1204891354@qq.com>
2022-12-01 15:05:36 +0800

    refactor: extract ProfileDropdown component from Header

--- src/header/header.js
+++ src/header/header.js
@@ -7,22 +7,26 @@ import "../ASSETS/font_icons/fonts.css";
 import logo from "../ASSETS/logo/logo.svg";
 import avatar from "../ASSETS/logo/avatar.jpg";
 
+function ProfileDropdown() {
+    return <Dropdown className="profile-channel">
+        <Dropdown.Item className="profile-item">
+            <i className="icon-id-card"></i>
+            Profile
+        </Dropdown.Item>
+        <Dropdown.Item className="profile-item">
+            <i className="icon-sign-in"></i>
+            Sign Out
+        </Dropdown.Item>
+    </Dropdown>;
+}
+
 class Header extends React.Component {
   render() {
     return (
       <header>
-        <img className="cruise-logo" src={logo} alt="logo" height="40px" />
-        <img className="profile-logo" src={avatar} alt="avatar" height="40px" />
-        <Dropdown className="profile-channel">
-          <Dropdown.Item className="profile-item">
-            <i className="icon-id-card"></i>
-            Profile
-          </Dropdown.Item>
-          <Dropdown.Item className="profile-item">
-            <i className="icon-sign-in"></i>
-            Sign Out
-          </Dropdown.Item>
-        </Dropdown>
+          <img className="cruise-logo" src={logo} alt="logo" height="40px"/>
+          <img className="profile-logo" src={avatar} alt="avatar" height="40px"/>
+          <ProfileDropdown/>
       </header>
     );
   }
