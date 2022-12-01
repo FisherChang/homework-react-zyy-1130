01a51bf9290099d6d51c80dfe48947a7aefea7f7
Yuyu Zhang <1204891354@qq.com>
2022-12-01 15:01:23 +0800

    refactor: move BasicInfo ResourceInfo getResource from agentDetails.js to functions.js

--- src/agent/details/agentDetails.js
+++ src/agent/details/agentDetails.js
@@ -1,44 +1,9 @@
 import React from "react";
 import "../../ASSETS/font_icons/fonts.css";
 import "./agentDetails.scss";
-
-function BasicInfo(props) {
-  return <div className="basic-info">
-    <i className="icon-desktop"></i>
-    <h4>{props.name}</h4>
-    <p className={`status-${props.status}`}>{props.status}</p>
-    <i className="icon-info"></i>
-    <p className="ip">{props.ip}</p>
-    <i className="icon-folder"></i>
-    <p className="location">{props.location}</p>
-  </div>;
-}
-
-function getResources(resources) {
-  return <div className="resource-list">
-    {resources.map((resource) => (
-        <div className="resource-item" key={resource}>
-          <span>{resource}</span>
-          <i className="icon-trash"></i>
-        </div>
-    ))}
-  </div>;
-}
-
-function ResourceInfo(props) {
-  return <div className="resources">
-    <i className="icon-plus"></i>
-    {getResources(props.resources)}
-    <div className="deny">
-      <i className="icon-deny"></i>
-      <span>Deny</span>
-    </div>
-  </div>;
-}
+import {BasicInfo, ResourceInfo} from "./functions";
 
 const AgentDetails = ({ agents }) => {
-  // console.log(agents);
-
   return (
     <section className="agent-list">
       {agents.map(({id, ip, location, name, os, resources, status}) => (
