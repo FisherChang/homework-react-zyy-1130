e7a22c72ed79b89a375ca025572159a24e8fabb2
Yuyu Zhang <1204891354@qq.com>
2022-12-02 18:01:19 +0800

    refactor: use fetchAgents function to fetch data from mock server

--- src/agent/api.js
+++ src/agent/api.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const URL='http://localhost:3001/agents';
 
-const fetchAgents=()=>axios.get(URL).then((response)=>response.data);
+const fetchAgents=()=>
+    axios.get(URL).then((response)=>response.data);
 
 export {fetchAgents};
