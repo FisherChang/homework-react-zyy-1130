063c11cef77f79f1857bce599309c80cd890631b
Yuyu Zhang <1204891354@qq.com>
2022-12-06 09:39:24 +0800

    refactor: extract component GetResources from ResourceInfo

--- src/agent/details/components/resourceInfo.js
+++ src/agent/details/components/resourceInfo.js
@@ -2,18 +2,22 @@ import React from "react";
 import "../../../ASSETS/font_icons/fonts.css";
 import "../agentDetails.scss";
 
+function GetResources(props) {
+    return <div className="resource-list">
+        {props.resources.map((resource) => (
+            <div className="resource-item" key={resource}>
+                <span>{resource}</span>
+                <i className="icon-trash"></i>
+            </div>
+        ))}
+    </div>;
+}
+
 export function ResourceInfo(props) {
     return (
         <div className="resources">
             <i className="icon-plus"></i>
-            <div className="resource-list">
-                {props.resources.map((resource) => (
-                    <div className="resource-item" key={resource}>
-                        <span>{resource}</span>
-                        <i className="icon-trash"></i>
-                    </div>
-                ))}
-            </div>
+            <GetResources resources={props.resources} />
             <div className="deny">
                 <i className="icon-deny"></i>
                 <span>Deny</span>
