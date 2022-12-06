9a861b9024b89636025b6cf4e065d5f525c03993
Yuyu Zhang <1204891354@qq.com>
2022-12-06 15:59:32 +0800

    feat: add Popup for agentItem

--- src/components/agent/components/details/components/resourceInfo.js
+++ src/components/agent/components/details/components/resourceInfo.js
@@ -3,11 +3,22 @@ import "../../../../../assets/font_icons/fonts.css";
 import "../agentDetails.scss";
 import {GetResources} from "./getResources";
 import {Deny} from "./deny";
+import Popup from "reactjs-popup";
+
+// import {updateResource} from "../../../api/api";
 
 export function ResourceInfo(props) {
+
     return (
         <div className="resources">
-            <i className="icon-plus"></i>
+            <Popup trigger={<i className="icon-plus"></i>} position="bottom center">
+                <div>
+                    <p>Separate multiple resource name with commas</p>
+                    <input placeholder="e.g.Chrome,Firefox"/>
+                    <button>Add Resources</button>
+                    <button>Cancel</button>
+                </div>
+            </Popup>
             <GetResources resources={props.resources}/>
             <Deny/>
         </div>
