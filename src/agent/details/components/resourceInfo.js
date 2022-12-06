9272d5c1430d9e37627a62dc7033cc58ca95cbc3
Yuyu Zhang <1204891354@qq.com>
2022-12-06 09:33:43 +0800

    refactor: rollback getResources function return to ResourceInfo

--- src/agent/details/components/resourceInfo.js
+++ src/agent/details/components/resourceInfo.js
@@ -2,24 +2,23 @@ import React from "react";
 import "../../../ASSETS/font_icons/fonts.css";
 import "../agentDetails.scss";
 
-function getResources(resources) {
-    return <div className="resource-list">
-        {resources.map((resource) => (
-            <div className="resource-item" key={resource}>
-                <span>{resource}</span>
-                <i className="icon-trash"></i>
-            </div>
-        ))}
-    </div>;
-}
-
 export function ResourceInfo(props) {
-    return <div className="resources">
-        <i className="icon-plus"></i>
-        {getResources(props.resources)}
-        <div className="deny">
-            <i className="icon-deny"></i>
-            <span>Deny</span>
+    return (
+        <div className="resources">
+            <i className="icon-plus"></i>
+            <div className="resource-list">
+                {props.resources.map((resource) => (
+                    <div className="resource-item" key={resource}>
+                        <span>{resource}</span>
+                        <i className="icon-trash"></i>
+                    </div>
+                ))}
+            </div>
+            <div className="deny">
+                <i className="icon-deny"></i>
+                <span>Deny</span>
+            </div>
         </div>
-    </div>;
+    );
+
 }
