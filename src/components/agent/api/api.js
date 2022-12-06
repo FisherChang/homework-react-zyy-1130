fd4845104fbf8ec3ea7f5771943dee70995a9a23
Yuyu Zhang <1204891354@qq.com>
2022-12-06 15:58:38 +0800

    feat: add updateResource function in api.js

--- src/components/agent/api/api.js
+++ src/components/agent/api/api.js
@@ -5,4 +5,8 @@ const URL='http://localhost:3001/agents';
 const fetchAgents=()=>
     axios.get(URL).then((response)=>response.data);
 
-export {fetchAgents};
+const updateResource=(id,newResources)=>
+    axios.patch(`${URL}/${id}`,{resources:`${newResources}`})
+        .then((response)=>response.data);
+
+export { fetchAgents, updateResource };
