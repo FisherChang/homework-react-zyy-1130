dfcb832a1501d58ecdca0616df18b5b920f9ed73
Yuyu Zhang <1204891354@qq.com>
2022-12-08 14:15:29 +0800

    refactor: delete resourceAddition

--- src/components/agent/components/details/components/resourceInfo.js
+++ src/components/agent/components/details/components/resourceInfo.js
@@ -3,13 +3,11 @@ import "../../../../../assets/font_icons/fonts.css";
 import "../agentDetails.scss";
 import { ResourceList } from "./resourceList";
 import { Deny } from "./deny";
-import { ResourceAddition } from "./resourceAddition";
 
 export function ResourceInfo(props) {
   return (
     <div className="resources">
-      <ResourceAddition id={props.id} resources={props.resources} />
-      <ResourceList resources={props.resources} />
+      <ResourceList resources={props.resources} id={props.id} />
       <Deny />
     </div>
   );
